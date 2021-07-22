bbc02674609af2062d7597f3c6085ee626776142
Alexis Jimenez <alexisjp26@gmail.com>
2021-07-22 17:10:09 -0600

    Proyecto Paso #3
    Correccion de errores.

--- service-suma/src/main/java/com/ucreativa/servicesuma/ServiceSumaApplication.java
+++ service-suma/src/main/java/com/ucreativa/servicesuma/ServiceSumaApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServiceSumaApplication {
 
 	public static void main(String[] args) {
@@ -14,8 +16,8 @@ public class ServiceSumaApplication {
 
 	@GetMapping("/suma/{numero1}/mas/{numero2}")
 	public String suma (@PathVariable String numero1, @PathVariable String numero2){
-		Integer result = Integer.parseInt(numero1) + Integer.parseInt(numero2);
-		return result.toString();
+		int result = Integer.parseInt(numero1) + Integer.parseInt(numero2);
+		return Integer.toString(result);
 	}
 
 }
